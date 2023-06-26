25d9c49e3bc45f072089ef188b1620da817a05b1
Eurus <eurus0601@gmail.com>
2023-06-26 17:26:38 +0800

    feat: Prevent page from scrolling when modal is active

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
+const modalMenu = document.querySelector(".modal");
+const body = document.querySelector("body");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("hamburger--active");
+  modalMenu.classList.toggle("modal--active");
+  body.classList.toggle("no-scroll");
 });
