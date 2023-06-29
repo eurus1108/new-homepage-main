e21de24e0acc2cbe357392b5e053a1ba4912b916
Eurus <eurus0601@gmail.com>
2023-06-29 13:39:06 +0800

    refactor: Improve code structure and optimize functionality

--- src/main.js
+++ src/main.js
@@ -1,11 +1,36 @@
 const hamburger = document.querySelector(".hamburger");
 const modalMenu = document.querySelector(".modal");
 const body = document.querySelector("body");
+let isClicked = false;
+
+let widthSize;
+
+window.onresize = window.onload = function () {
+  widthSize = this.innerHeight;
+
+  if (widthSize >= 688) {
+    body.classList.remove("no-scroll");
+  }
+
+  if (widthSize < 688 && modalMenu.classList.contains("modal--active")) {
+    body.classList.add("no-scroll");
+  }
+};
 
 hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("hamburger--active");
+  if (!isClicked) {
+    isClicked = true;
+    hamburger.classList.add("hamburger--active");
+    hamburger.setAttribute("aria-expanded", isClicked);
+    body.classList.add("no-scroll");
+  } else {
+    isClicked = false;
+    hamburger.classList.remove("hamburger--active");
+    hamburger.setAttribute("aria-expanded", isClicked);
+    body.classList.remove("no-scroll");
+  }
+
   modalMenu.classList.toggle("modal--active");
-  body.classList.toggle("no-scroll");
 });
 
 const modalLink = document.querySelectorAll(".new-section-link");
